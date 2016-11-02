215350bc7947c688667d796d8e8d04ac569bdfd4
Nicolas Meylan <nicolas.meylan@mineral.software>
2016-11-02 22:22:01 +0100

    Replace uglifier with closure-compiler

--- lib/jekyll/assets/hooks/compression.rb
+++ lib/jekyll/assets/hooks/compression.rb
@@ -10,16 +10,20 @@ Jekyll::Assets::Hook.register :env, :init do
   end
 
   if compress?("js")
-    try_require "uglifier" do
-      if !jekyll.safe && (opts = self.asset_config.fetch("external", {}).fetch("uglifier", nil))
-        self.js_compressor = Uglifier.new(
-          opts.symbolize_keys
-        )
+    # try_require "uglifier" do
+    #   if !jekyll.safe && (opts = self.asset_config.fetch("external", {}).fetch("uglifier", nil))
+    #     self.js_compressor = Uglifier.new(
+    #       opts.symbolize_keys
+    #     )
+    #
+    #   else
+    #     self.js_compressor = \
+    #       :uglify
+    #   end
+    # end
+    try_require "closure-compiler" do
+      self.js_compressor = Closure::Compiler.new(compilation_level: 'ADVANCED_OPTIMIZATIONS')
 
-      else
-        self.js_compressor = \
-          :uglify
-      end
     end
   end
 end
